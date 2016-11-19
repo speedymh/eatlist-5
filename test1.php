022c77fdef1158c70d59661f53d1b8c68f8a07c9
speedymh <matthias_hoppe@outlook.de>
2016-11-19 17:36:15 +0100

    in php erster versuch mit sql-register

--- test1.php
+++ test1.php
@@ -6,6 +6,26 @@
 <body>
 	<?php 
 		echo "Hallo " . $_POST['login-field-name'] . "! :)";
+	
+	
+		if (isset($_POST["register"])) {
+			$con = mysqli_connect("", "root", "Mi1wds3hL");
+			mysqli_select_db($con, "eatlist");
+			
+			$sql = "INSERT INTO benutzerdaten(benutzername, passwort) values('" . $_POST["register-field-name"] . "', '"
+			 . $_POST["register-field-pw"] . "')";
+			
+			mysqli_query($con, $sql);
+			
+			$num = mysqli_affected_rows($con);
+			if($num>0) {
+				echo "<p><font color = '#00aa00'>Ein Datensatz hinzugekommen</font></p>";
+			} else {
+				echo "<p><font color='#ff0000'>Es ist ein Fehler aufgetreten, es ist kein Datensatz hinzugekommen</font></p>"
+			}
+			
+			mysqli_close($con);
+		}
 	?>
 </body>
 </html>
