76661e47baedbba93037c4fda3dc27788ab274a8
speedymh <matthias_hoppe@outlook.de>
2016-11-20 14:50:25 +0100

    bei register wird datensatz hinzugefügt

--- test1.php
+++ test1.php
@@ -2,13 +2,9 @@
 <html>
 <head>
 	<meta charset = "uft-8">
-</head>
-<body>
-	<?php 
-		echo "Hallo " . $_POST['login-field-name'] . "! :)";
-	
-	
-		if (isset($_POST["register"])) {
+	<?php 	
+		if (isset($_POST["register"])) 
+		{
 			$con = mysqli_connect("", "root", "Mi1wds3hL");
 			mysqli_select_db($con, "eatlist");
 			
@@ -21,11 +17,17 @@
 			if($num>0) {
 				echo "<p><font color = '#00aa00'>Ein Datensatz hinzugekommen</font></p>";
 			} else {
-				echo "<p><font color='#ff0000'>Es ist ein Fehler aufgetreten, es ist kein Datensatz hinzugekommen</font></p>"
+				echo "<p><font color='#ff0000'>Es ist ein Fehler aufgetreten, es ist kein Datensatz hinzugekommen</font></p>";
 			}
 			
 			mysqli_close($con);
 		}
 	?>
+</head>
+<body>
+	<?php 
+		echo "HEy there";
+	?>
+	<p>hi</p>
 </body>
 </html>
