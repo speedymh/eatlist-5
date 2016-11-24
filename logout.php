53648569bb74522f67b4dd6f4460d32985a2d170
speedymh <matthias_hoppe@outlook.de>
2016-11-24 22:01:28 +0100

    logout.php hinzugefügt

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php 
+	session_start();
+	session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="uft-8">
+	</head>
+<body>
+	<meta http-equiv="refresh" content="1; url=index.php"/>
+</body>
+</html>
